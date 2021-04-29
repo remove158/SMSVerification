0e5ba8f43693934dba9a82cec1e05cc18c0385a4
remove158 <remove158@gmail.com>
2021-04-29 18:52:43 +0700

    fix(req) : add bodyparser and cors

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app  = express();
 const smsRoute = require('./routes/sms.route')
 const errorHandle = require("./controllers/errorHandle");
+const  cors = require('cors')
 
+app.use(cors({ origin: true })) // to enable cors 
+app.use(express.json()); // to enable body parse
 //-------------------------------------------------------------------------//
 // TODO : Handle all routes  
 //-------------------------------------------------------------------------//
