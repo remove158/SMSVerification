29c938f02a2c98c435a6b2899942ec600287f38d
remove158 <remove158@gmail.com>
2021-04-29 19:18:22 +0700

    feat(sms) : add all functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,28 @@ import firebase from "./firebase/config";
 import axios from "axios";
 
 function App() {
-	const [state, setstate] = useState({ sessionInfo: "", phoneNumber: ""  , code:""});
+	const [state, setstate] = useState({
+		sessionInfo: "",
+		phoneNumber: "",
+		code: "",
+	});
 
 	window.myCallback = useCallback(
 		async (recaptchaToken) => {
 			const url = "http://localhost:3001/sendSMS"; // <- put your endpoint here
 			const phoneNumber = "+66" + state.phoneNumber.substring(1);
-            console.log(phoneNumber , recaptchaToken);
+			console.log(phoneNumber, recaptchaToken);
 			try {
 				const response = await axios.post(url, {
 					phoneNumber,
 					recaptchaToken,
 				});
-				//console.log("SUCCSESS", response.data.sessionInfo);
+				// handle if send sucess
+				alert("Sended");
 				setstate({ ...state, sessionInfo: response.data.sessionInfo });
 			} catch (err) {
+				// handle if send faile
+				alert("Error to send !");
 				console.error(err.response.data.message);
 			}
 		},
@@ -42,6 +49,21 @@ function App() {
 			window.recaptchaWidgetId = widgetId;
 		});
 	}, []);
+
+	const verifycode = async () => {
+		try {
+			const url = "http://localhost:3001/verifySMS"; // <- put your endpoint here
+			const result = await axios.post(url, {
+				verificationCode: state.code,
+				sessionInfo: state.sessionInfo,
+			});
+			//handle if code is valid
+			alert(`verify success token : ${result.data.accessToken}`);
+		} catch (err) {
+			//handle if code is invalid
+			alert(err);
+		}
+	};
 	return (
 		<div className="App">
 			<div className="send-sms-form">
@@ -55,8 +77,8 @@ function App() {
 				></input>
 				<button id="sendCode">Send SMS</button>
 			</div>
-            <div className="Verify CODE">
-            <input
+			<div className="Verify CODE">
+				<input
 					type="text"
 					label="phonenumber"
 					value={state.code}
@@ -64,8 +86,10 @@ function App() {
 						setstate({ ...state, code: e.target.value })
 					}
 				></input>
-				<button id="verify">Verify</button>
-            </div>
+				<button id="verify" onClick={verifycode}>
+					Verify
+				</button>
+			</div>
 
 			<pre>{JSON.stringify(state, null, 2)}</pre>
 		</div>
